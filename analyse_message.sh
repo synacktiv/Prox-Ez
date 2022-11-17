50e20184e7c65e782fb2d56a9d836fa2d96da2f2
Pierre Milioni <pierre.milioni@synacktiv.com>
2022-11-17 18:35:37 +0100

    Add simple bash script to see GSS API messages in Wireshark
    Just listen on loopback, run the script and analyze the GSS API
    messages from there.

---
+++ analyse_message.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Provides an easy way to analyse a GSS-API formatted
+# message in wireshark.
+
+# Usage example:
+# ./analyze_message.sh 'Negotiate YIIEoQYGKwYBBQUCoIIElTCCBJGg...'
+# See the results on wireshark by looking at the loopback interface.
+
+port=$2
+if [ -z "$2" ]; then
+	port=8080
+fi
+
+nc -nvlp $port &
+echo -ne 'GET / HTTP/1.1\r\nHost: Test\r\nAuthorization: '$3'\r\n\r\n' | timeout 1 nc -v 127.0.0.1 $port
+nc -nvlp $port &
+echo -ne 'HTTP/1.1 401 Unauthorized\r\nWWW-Authenticate: '$3'\r\n\r\n' | timeout 1 nc -v 127.0.0.1 $port
