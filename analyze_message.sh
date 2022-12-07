7076d668ae4f44940cda997143baee462a0d2bff
Pierre Milioni - Synacktiv <90333807+pm-syn@users.noreply.github.com>
2022-12-07 11:32:28 +0100

    Fix wrong param in analyze_message.sh

--- analyze_message.sh
+++ analyze_message.sh
@@ -13,6 +13,6 @@ if [ -z "$2" ]; then
 fi
 
 nc -nvlp $port &
-echo -ne 'GET / HTTP/1.1\r\nHost: Test\r\nAuthorization: '$3'\r\n\r\n' | timeout 1 nc -v 127.0.0.1 $port
+echo -ne 'GET / HTTP/1.1\r\nHost: Test\r\nAuthorization: '$1'\r\n\r\n' | timeout 1 nc -v 127.0.0.1 $port
 nc -nvlp $port &
-echo -ne 'HTTP/1.1 401 Unauthorized\r\nWWW-Authenticate: '$3'\r\n\r\n' | timeout 1 nc -v 127.0.0.1 $port
+echo -ne 'HTTP/1.1 401 Unauthorized\r\nWWW-Authenticate: '$1'\r\n\r\n' | timeout 1 nc -v 127.0.0.1 $port
